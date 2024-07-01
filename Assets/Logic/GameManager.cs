37ac7a289617ad95315a528ce12c59b7763626f8
Manuel Mindic <114492127+manuelmindic@users.noreply.github.com>
2024-07-02 01:12:39 +0200

    Revert "#3 Pull Request: Pull Gamelogic into main"

--- Assets/Logic/GameManager.cs
+++ Assets/Logic/GameManager.cs
@@ -1,8 +1,15 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    private GameManager _gameManager;
+
+    public GameManager(GameManager gameManager)
+    {
+        _gameManager = gameManager;
+    }
+
+    public GameManager GetGameManager() { return _gameManager; }
 }
